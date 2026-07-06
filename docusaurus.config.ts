725100a94ab9b49251c54cb091c70f484c4f56a7
Aman Oli <oliaman109@gmail.com>
2026-07-06 14:25:50 +0545

    made the docs editable

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -47,8 +47,8 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl:
+            'https://github.com/amanoli11/Australia-Handover-Document/blob/main/',
         },
         blog: {
           showReadingTime: true,
@@ -58,8 +58,8 @@ const config: Config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl:
+            'https://github.com/amanoli11/Australia-Handover-Document/blob/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: "warn",
           onInlineAuthors: "warn",
