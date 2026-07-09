5bd2513939fc5646beef2058f181177403f1b4ad
Aman Oli <oliaman109@gmail.com>
2026-07-09 22:08:21 +0545

    hidden customer portal features

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,6 +44,7 @@ const config: Config = {
       "classic",
       {
         docs: {
+          // exclude: ["**/hidden/**"],
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
